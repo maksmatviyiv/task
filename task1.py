f5e14cc09f4191068347afb85d6853b824da324a
maksmatviyiv <maksym.matviyiv1998@gmail.com>
2018-11-12 17:32:51 +0200

    fix small bugs

--- task1.py
+++ task1.py
@@ -1,11 +1,18 @@
-def task(number):
+def task(number: int):
+  """returns the number of bits that are equal to one in the binary representation of that number."""
     
-    binary = bin(number)
-    
+    binary = bin(number)    
     counter = 0
-    for i in binary:
-        if (i == "1"):
+    number1 = binary[2:]
+    for i in number1: 
+        i = int(i)
+        if i == 1:
             counter = counter + 1
-    print(counter)
-n = int(input("Введіть число "))
-task(n)
+    return counter
+
+def main():
+    n = int(input("Введіть число "))
+    print(task(n))
+
+if __name__ =='__main__':
+    main()
